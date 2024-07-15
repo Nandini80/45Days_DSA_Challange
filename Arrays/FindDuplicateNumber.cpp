7ff00a45e4704392f890c94ff40a81fc47792ab2
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-15 08:20:21 +0530

    Find Dup Number

---
+++ Arrays/FindDuplicateNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+      map<int,int> mp;
+       for(int i=0 ; i<nums.size() ; i++)
+       {
+         mp[nums[i]]++;
+         if(mp[nums[i]]>1)
+         return nums[i];
+       } 
+       return -1;    
+    }
+};
