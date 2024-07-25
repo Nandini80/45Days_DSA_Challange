043e24c85f3065fe836e06eea4ece4bd3253574b
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-25 07:22:08 +0530

    Basic calculator

---
+++ Strings/BasicCalculator.cpp
@@ -0,0 +1,45 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int calculate(string s) {
+        s += '+';  
+        stack<int> stk; 
+        
+        long long ans = 0, curr = 0;
+        char sign = '+'; 
+        
+        for(int i = 0; i < s.size(); i++) {
+            if (isdigit(s[i])) {
+                curr = curr * 10 + (s[i] - '0');
+            } 
+            else if (s[i] == ' ') {
+                continue; 
+            }
+            else if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+                if (sign == '+') {
+                    stk.push(curr); 
+                } else if (sign == '-') {
+                    stk.push(curr * -1);
+                } else if (sign == '*') {
+                    int num = stk.top(); stk.pop(); 
+                    stk.push(num * curr); 
+                } else if (sign == '/') {
+                    int num = stk.top(); stk.pop(); 
+                    stk.push(num / curr); 
+                }
+                
+                curr = 0; 
+                sign = s[i]; 
+            }
+        }
+        
+        while (!stk.empty()) {
+            ans += stk.top(); stk.pop();
+        }
+            
+        return ans;    
+    }
+};
