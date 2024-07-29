f06e8d16782881c7e860e3ad52938717c9d8b08b
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-29 09:11:04 +0530

    Permutations In array

---
+++ Mathematical/PermutationsInArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+  public:
+    bool isPossible(long long a[], long long b[], int n, long long k) 
+    {
+        sort(a, a + n);
+        sort(b, b + n, greater<int>());
+        
+        for (int i = 0; i < n; i++)
+        if (a[i] + b[i] < k)
+            return false;
+
+        return true;
+    }
+};
